f13ccd5b56e421fb8b24c03e5b3396a91ffca4cb
Hampus Fröjdholm <hampus.frojdholm@gmail.com>
2020-05-09 15:28:39 +0200

    ImageBlock API
    The ImageBlock API is in preparation for multithreading to be able to
    divide an image into blocks.

--- rayrs-lib/src/image.rs
+++ rayrs-lib/src/image.rs
@@ -9,6 +9,46 @@ pub enum ImageFormat {
     PpmBinary,
 }
 
+pub struct ImageBlock {
+    offset_x: usize,
+    offset_y: usize,
+    image: Image,
+}
+
+impl ImageBlock {
+    fn new(width: usize, height: usize, offset_x: usize, offset_y: usize) -> ImageBlock {
+        ImageBlock {
+            offset_x,
+            offset_y,
+            image: Image::new(width, height),
+        }
+    }
+
+    pub fn offset_x(&self) -> usize {
+        self.offset_x
+    }
+
+    pub fn offset_y(&self) -> usize {
+        self.offset_y
+    }
+
+    pub fn width(&self) -> usize {
+        self.image.width()
+    }
+
+    pub fn height(&self) -> usize {
+        self.image.height()
+    }
+
+    pub fn pixel(&self, i: usize, j: usize) -> Vec3 {
+        self.image.pixel(i, j)
+    }
+
+    pub fn set_pixel(&mut self, i: usize, j: usize, val: Vec3) {
+        self.image.set_pixel(i, j, val)
+    }
+}
+
 pub struct Image {
     width: usize,
     height: usize,
@@ -24,6 +64,97 @@ impl Image {
         }
     }
 
+    pub fn blocks(
+        width: usize,
+        height: usize,
+        block_size: usize,
+    ) -> (usize, usize, Vec<ImageBlock>) {
+        assert!(width > block_size && height > block_size);
+        let mut images = Vec::new();
+
+        let mut height_left = height as i32;
+        let mut width_left = width as i32;
+
+        while height_left > 0 {
+            while width_left > 0 {
+                let offset_x = width - width_left as usize;
+                let offset_y = height - height_left as usize;
+
+                if width_left < block_size as i32 {
+                    if height_left < block_size as i32 {
+                        images.push(ImageBlock::new(
+                            width_left as usize,
+                            height_left as usize,
+                            offset_x,
+                            offset_y,
+                        ));
+                    } else {
+                        images.push(ImageBlock::new(
+                            width_left as usize,
+                            block_size,
+                            offset_x,
+                            offset_y,
+                        ));
+                    }
+                } else {
+                    if height_left < block_size as i32 {
+                        images.push(ImageBlock::new(
+                            block_size,
+                            height_left as usize,
+                            offset_x,
+                            offset_y,
+                        ));
+                    } else {
+                        images.push(ImageBlock::new(block_size, block_size, offset_x, offset_y));
+                    }
+                }
+                width_left -= block_size as i32;
+            }
+            height_left -= block_size as i32;
+            width_left = width as i32;
+        }
+
+        let x_size = if width % block_size == 0 {
+            width / block_size
+        } else {
+            width / block_size + 1
+        };
+
+        let y_size = if height % block_size == 0 {
+            height / block_size
+        } else {
+            height / block_size + 1
+        };
+        assert!(x_size * y_size == images.len());
+        (x_size, y_size, images)
+    }
+
+    pub fn from_blocks(
+        blocks: &[ImageBlock],
+        width: usize,
+        height: usize,
+        pix_width: usize,
+        pix_height: usize,
+    ) -> Image {
+        assert!(width * height == blocks.len());
+
+        let mut pixels = vec![Vec3::new(0.0, 0.0, 0.0); pix_width * pix_height];
+
+        for block in blocks {
+            for i in 0..block.height() {
+                for j in 0..block.width() {
+                    pixels[(i + block.offset_y) * pix_height + j + block.offset_x] =
+                        block.pixel(i, j);
+                }
+            }
+        }
+
+        Image {
+            width: pix_width,
+            height: pix_height,
+            image: pixels,
+        }
+    }
 
     pub fn width(&self) -> usize {
         self.width
@@ -48,9 +179,9 @@ impl Image {
             .iter()
             .fold(Vec::with_capacity(self.image.len() * 3), |mut acc, val| {
                 let pixel = val.clip(0., 1.).pow(gamma);
-            let r = (255.99 * pixel.x()) as u8;
-            let g = (255.99 * pixel.y()) as u8;
-            let b = (255.99 * pixel.z()) as u8;
+                let r = (255.99 * pixel.x()) as u8;
+                let g = (255.99 * pixel.y()) as u8;
+                let b = (255.99 * pixel.z()) as u8;
                 acc.push(r);
                 acc.push(g);
                 acc.push(b);
